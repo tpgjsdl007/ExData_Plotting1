504e04e8b6548c02bfeb194b0fdaea1bd9be99c7
tpgjsdl007 <52301293+tpgjsdl007@users.noreply.github.com>
2019-09-14 20:15:17 -0700

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,10 @@
+# Plot 4
+par(mfrow=c(2,2))
+par(mar=c(4,4,4,4))
+plot(hpc_sub$dt, hpc_sub$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(hpc_sub$dt, hpc_sub$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(hpc_sub$dt, hpc_sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+points(hpc_sub$dt, hpc_sub$Sub_metering_2, type = "l", col = "red")
+points(hpc_sub$dt, hpc_sub$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", cex = 0.8)
+plot(hpc_sub$dt, hpc_sub$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
